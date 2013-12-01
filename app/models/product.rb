603b3edb2f397136f317e263e12b78acee08943a
Garrett A. Enquist <garrett.enquist@gmail.com>
2013-12-01 13:06:59 -0600

    Added product pictures

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ActiveRecord::Base
-  attr_accessible :description, :name, :price, :stock_quantity
+  attr_accessible :description, :name, :price, :stock_quantity, :image_filename
   validates :description, :name, :price, :stock_quantity, presence: true
   has_many :lineitems
   has_many :orders, through: :lineitems
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131128195023) do
+ActiveRecord::Schema.define(:version => 20131201182532) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -84,6 +84,7 @@ ActiveRecord::Schema.define(:version => 20131128195023) do
     t.integer  "stock_quantity"
     t.datetime "created_at",     :null => false
     t.datetime "updated_at",     :null => false
+    t.string   "image_filename"
   end
 
   create_table "provinces", :force => true do |t|
